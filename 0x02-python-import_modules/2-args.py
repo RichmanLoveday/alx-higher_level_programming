5d94799ce9741f0bdcbec68170c3bd1ff90cb951
RichmanLoveday <lovedayrichman@yahoo.com>
2023-11-03 07:42:04 +0100

    cleared new line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 import sys
 if __name__ == "__main__":
     args_num = len(sys.argv) - 1
-
     print(
             "{:d} argument".format(args_num)
             if args_num == 1 else "{:d} arguments".format(args_num), end=""
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
     arguments = len(sys.argv) - 1
     total = 0
-
     for i in range(arguments):
         total += int(sys.argv[i + 1])
 
